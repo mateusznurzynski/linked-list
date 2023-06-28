237ec7be0c0254ad6b0448b8caae73397d9096bf
Mateusz Nurzyński <mateonurzynski@gmail.com>
2023-06-28 16:11:38 +0200

    Create a method that prepends a new node

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ const LinkedList = function LinkedList() {
     size: 0,
     head: null,
     tail: null,
+    prepend(value = null) {
+      if (this.head === null && this.tail === null) {
+        const newNode = Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        return newNode;
+      } else {
+        const oldHead = this.head;
+        const newNode = Node(value, oldHead);
+        this.head = newNode;
+        return newNode;
+      }
+    },
   };
 };
 
@@ -12,3 +25,5 @@ const Node = function Node(value = null, nextNode = null) {
     nextNode,
   };
 };
+
+const list = LinkedList();
