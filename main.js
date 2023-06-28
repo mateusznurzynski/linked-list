c4c186d98925f601c53c5c19c7a218eacef58a56
Mateusz Nurzyński <mateonurzynski@gmail.com>
2023-06-28 16:01:53 +0200

    Create factory function for linked lists and nodes

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const LinkedList = function LinkedList() {
+  return {
+    size: 0,
+    head: null,
+    tail: null,
+  };
+};
 
+const Node = function Node(value = null, nextNode = null) {
+  return {
+    value,
+    nextNode,
+  };
+};
